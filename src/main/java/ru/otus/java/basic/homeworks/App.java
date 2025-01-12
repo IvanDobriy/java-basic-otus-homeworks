0199121b3478527b4fcb67590468e567eb32b6f1
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2025-01-12 19:16:10 +0300

    initial commit

---
+++ src/main/java/ru/otus/java/basic/homeworks/App.java
@@ -0,0 +1,10 @@
+package ru.otus.java.basic.homeworks;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ru/otus/java/basic/homeworks/AppTest.java
@@ -0,0 +1,21 @@
+package ru.otus.java.basic.homeworks;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+/**
+ * Unit test for simple App.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

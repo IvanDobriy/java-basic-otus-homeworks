271013734804d709770a636dec939285e4d8fdd5
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2025-02-17 15:30:00 +0300

    prepare Horse

--- src/main/java/ru/otus/java/basic/homeworks/lesson7/transport/Horse.java
+++ src/main/java/ru/otus/java/basic/homeworks/lesson7/transport/Horse.java
@@ -2,20 +2,35 @@ package ru.otus.java.basic.homeworks.lesson7.transport;
 
 import ru.otus.java.basic.homeworks.lesson7.landscape.Landscape;
 
+import java.util.Objects;
+
 public class Horse extends ATransport {
+    private int forces;
 
     @Override
     public int spendEnergy(int amount) {
-        return 0;
+        if (amount < 0) {
+            throw new IllegalArgumentException("amount < 0");
+        }
+        int remains = forces - amount;
+        forces = Math.max(0, remains);
+        return remains;
     }
 
     @Override
     public int getEnergy() {
-        return 0;
+        return forces;
     }
 
     @Override
     public boolean move(int distance, Landscape landscape) {
-        return false;
+        if (Objects.isNull(this.getDriver())) {
+            return false;
+        }
+        if (landscape.equals(Landscape.SWAMP)) {
+            return false;
+        }
+        int remains = spendEnergy(distance);
+        return remains >= 0;
     }
 }

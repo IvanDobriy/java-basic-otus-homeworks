1ad64eafee644270f335a463c8d6732ffe612f58
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2025-03-13 09:42:23 +0300

    add person getters and setters

--- src/main/java/ru/otus/java/basic/homeworks/lesson11/enitity/Person.java
+++ src/main/java/ru/otus/java/basic/homeworks/lesson11/enitity/Person.java
@@ -1,7 +1,64 @@
 package ru.otus.java.basic.homeworks.lesson11.enitity;
 
+import java.util.Objects;
+
 public class Person {
     String name;
+
     Position position;
+
     Long id;
+
+    public Person() {
+        name = null;
+        position = null;
+        id = null;
+    }
+
+    public Person(String name, Position position, Long id) {
+        this.name = name;
+        this.position = position;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && position == person.position && Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position, id);
+    }
 }

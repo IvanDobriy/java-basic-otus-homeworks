aa17c1b9e463d2cf767ea6ffbafbaed0d18c2d5c
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2025-04-27 18:04:25 +0300

    create classes

---
+++ src/main/java/ru/otus/java/basic/homeworks/lesson19/App.java
@@ -0,0 +1,6 @@
+package ru.otus.java.basic.homeworks.lesson19;
+
+public class App {
+
+
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/lesson19/Fruit.java
@@ -0,0 +1,5 @@
+package ru.otus.java.basic.homeworks.lesson19;
+
+public abstract class Fruit {
+    public abstract double size();
+}

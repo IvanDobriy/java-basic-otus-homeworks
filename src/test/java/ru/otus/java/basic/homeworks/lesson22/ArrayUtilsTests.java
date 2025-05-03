98017801a77ddbbb52f4d2f45aa7226591126386
idanilchenko@altsoft.biz <dobrogovremeny@gmail.com>
2025-05-03 16:40:23 +0300

    fix remarks

--- src/test/java/ru/otus/java/basic/homeworks/lesson22/ArrayUtilsTests.java
+++ src/test/java/ru/otus/java/basic/homeworks/lesson22/ArrayUtilsTests.java
@@ -15,13 +15,7 @@ import java.util.stream.Stream;
 public class ArrayUtilsTests {
     @ParameterizedTest()
     @MethodSource("getArrData")
-    void lastAfter1Test(int[] arr, int[] expected, Class<Exception> exception) {
-        if (exception != null) {
-            Assertions.assertThrows(exception, () -> {
-                ArrayUtils.lastAfter1(arr);
-            });
-            return;
-        }
+    void lastAfter1Test(int[] arr, int[] expected) {
         final var result = ArrayUtils.lastAfter1(arr);
         Assertions.assertArrayEquals(expected, result);
     }
@@ -29,23 +23,30 @@ public class ArrayUtilsTests {
     static Stream<Arguments> getArrData() {
         return List.of(
                 Arguments.arguments(new int[]{1, 2, 3}, new int[]{2, 3}, null),
-                Arguments.arguments(new int[]{2, 1, 3}, new int[]{3}, null),
-                Arguments.arguments(new int[]{}, null, RuntimeException.class),
-                Arguments.arguments(new int[]{1}, null, RuntimeException.class),
-                Arguments.arguments(new int[]{2}, null, RuntimeException.class),
-                Arguments.arguments(new int[]{2, 1}, null, RuntimeException.class)
+                Arguments.arguments(new int[]{2, 1, 3}, new int[]{3}, null)
+        ).stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("getErrArrData")
+    void lastAfter1ErrTest(int[] arr, Class<Exception> exception) {
+        Assertions.assertThrows(exception, () -> {
+            ArrayUtils.lastAfter1(arr);
+        });
+    }
+
+    static Stream<Arguments> getErrArrData() {
+        return List.of(
+                Arguments.arguments(new int[]{}, RuntimeException.class),
+                Arguments.arguments(new int[]{1}, RuntimeException.class),
+                Arguments.arguments(new int[]{2}, RuntimeException.class),
+                Arguments.arguments(new int[]{2, 1}, RuntimeException.class)
         ).stream();
     }
 
     @ParameterizedTest
     @MethodSource("getCheckData")
-    void checkInTest(Set<Integer> set, int[] arr, boolean expected, Class<Exception> exceptionClass) {
-        if (exceptionClass != null) {
-            Assertions.assertThrows(exceptionClass, () -> {
-                ArrayUtils.checkIn(set, arr);
-            });
-            return;
-        }
+    void checkInTest(Set<Integer> set, int[] arr, boolean expected) {
         boolean result = ArrayUtils.checkIn(set, arr);
         Assertions.assertEquals(expected, result);
     }
@@ -54,8 +55,20 @@ public class ArrayUtilsTests {
         return List.of(
                 Arguments.arguments(Set.of(1, 2), new int[]{1, 2, 3}, true, null),
                 Arguments.arguments(Set.of(1, 2, 3), new int[]{1, 2, 3}, true, null),
-                Arguments.arguments(Set.of(1), new int[]{2, 3}, false, null),
-                Arguments.arguments(Set.of(1, 2, 3, 4), new int[]{1, 2, 3}, false, IllegalArgumentException.class)
+                Arguments.arguments(Set.of(1), new int[]{2, 3}, false, null)).stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("getCheckErrData")
+    void checkInErrTest(Set<Integer> set, int[] arr, Class<Exception> exceptionClass) {
+        Assertions.assertThrows(exceptionClass, () -> {
+            ArrayUtils.checkIn(set, arr);
+        });
+    }
+
+    static Stream<Arguments> getCheckErrData() {
+        return List.of(
+                Arguments.arguments(Set.of(1, 2, 3, 4), new int[]{1, 2, 3}, IllegalArgumentException.class)
         ).stream();
     }
 }
